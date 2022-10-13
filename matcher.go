a04a54dfa55f774295abecc010b567aa4d235653
Nathan Witmer <nathan@zerowidth.com>
2022-10-12 22:09:45 -0600

    match urls

--- matcher.go
+++ matcher.go
@@ -4,15 +4,31 @@ import (
 	"regexp"
 )
 
+type Reference interface {
+}
+
 type Issue struct {
 	Owner string
 	Repo  string
 	Num   string
 }
 
+type Pull struct {
+	Owner string
+	Repo  string
+	Num   string
+}
+
+type Discussion struct {
+	Owner string
+	Repo  string
+	Num   string
+}
+
+var nwoReferencePattern = regexp.MustCompile(`https://github.com/([^/]+)/([^/]+)/(issue|pull|discussions)/(\d+)(.*)`)
 var issueReferencePattern = regexp.MustCompile(`\b([^/]+)/([^/#]+)#(\d+)\b`)
 
-func matchIssue(input string) (Issue, bool) {
+func matchIssueReference(input string) (Issue, bool) {
 	matches := issueReferencePattern.FindStringSubmatch(input)
 	if matches != nil {
 		return Issue{
@@ -23,4 +39,34 @@ func matchIssue(input string) (Issue, bool) {
 	}
 
 	return Issue{}, false
+}
+
+func matchURL(url string) (Reference, bool) {
+	matches := nwoReferencePattern.FindStringSubmatch(url)
+	if matches == nil {
+		return nil, false
+	}
+
+	switch matches[3] {
+	case "issue":
+		return Issue{
+			Owner: matches[1],
+			Repo:  matches[2],
+			Num:   matches[4],
+		}, true
+	case "pull":
+		return Pull{
+			Owner: matches[1],
+			Repo:  matches[2],
+			Num:   matches[4],
+		}, true
+	case "discussions":
+		return Discussion{
+			Owner: matches[1],
+			Repo:  matches[2],
+			Num:   matches[4],
+		}, true
+	}
+
+	return nil, false
 }
--- matcher_test.go
+++ matcher_test.go
@@ -21,7 +21,7 @@ func TestMatchIssue(t *testing.T) {
 		},
 		{
 			name:     "reference inside markdown link",
-			input:    "[owner/repo#123](owner/repo#123: title)",
+			input:    "[owner/repo#123: title](https://github.com/owner/repo/issues/123)",
 			expected: Issue{Owner: "owner", Repo: "repo", Num: "123"},
 		},
 		{
@@ -31,12 +31,59 @@ func TestMatchIssue(t *testing.T) {
 	} {
 		test := test
 		t.Run(test.name, func(t *testing.T) {
-			issue, matched := matchIssue(test.input)
+			issue, matched := matchIssueReference(test.input)
 			if (test.expected == Issue{}) {
 				assert.False(t, matched)
 			} else {
 				assert.True(t, matched)
 				assert.Equal(t, test.expected, issue)
+			}
+		})
+	}
+}
+
+type urlTest struct {
+	name     string
+	url      string
+	expected Reference
+}
+
+func TestMatchURL(t *testing.T) {
+	for _, test := range []urlTest{
+		{
+			name:     "issue url",
+			url:      "https://github.com/owner/repo/issue/123",
+			expected: Issue{Owner: "owner", Repo: "repo", Num: "123"},
+		},
+		{
+			name:     "issue url inside markdown",
+			url:      "[owner/repo#123: title](https://github.com/owner/repo/issue/123)",
+			expected: Issue{Owner: "owner", Repo: "repo", Num: "123"},
+		},
+		{
+			name:     "pull url",
+			url:      "https://github.com/owner/repo/pull/123",
+			expected: Pull{Owner: "owner", Repo: "repo", Num: "123"},
+		},
+		{
+			name:     "pull url inside markdown",
+			url:      "[owner/repo#123: title](https://github.com/owner/repo/pull/123)",
+			expected: Pull{Owner: "owner", Repo: "repo", Num: "123"},
+		},
+		{
+			name:     "repo discussion url",
+			url:      "https://github.com/owner/repo/discussions/123",
+			expected: Discussion{Owner: "owner", Repo: "repo", Num: "123"},
+		},
+	} {
+		test := test
+		t.Run(test.name, func(t *testing.T) {
+			reference, matched := matchURL(test.url)
+			if (test.expected == Issue{}) {
+				assert.False(t, matched)
+			} else {
+				assert.True(t, matched)
+				assert.Equal(t, test.expected, reference)
 			}
 		})
 	}
