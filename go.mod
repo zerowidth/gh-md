083d5cf2498534680bd042b7b777cac7dcb98ee5
Nathan Witmer <nathan@zerowidth.com>
2022-10-15 15:20:40 -0600

    Sketch out CLI interface including flags

--- go.mod
+++ go.mod
@@ -2,7 +2,11 @@ module github.com/zerowidth/gh-md
 
 go 1.19
 
-require github.com/cli/go-gh v0.1.1
+require (
+	github.com/cli/go-gh v0.1.1
+	github.com/spf13/cobra v1.5.0
+	github.com/stretchr/testify v1.8.0
+)
 
 require (
 	github.com/aymanbagabas/go-osc52 v1.0.3 // indirect
@@ -11,17 +15,14 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/henvic/httpretty v0.1.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/kr/text v0.2.0 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/muesli/termenv v0.13.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.4.2 // indirect
-	github.com/spf13/cobra v1.5.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/stretchr/objx v0.4.0 // indirect
-	github.com/stretchr/testify v1.8.0 // indirect
 	github.com/thlib/go-timezone-local v0.0.0-20210907160436-ef149e42d28e // indirect
 	golang.org/x/net v0.0.0-20221002022538-bcab6841153b // indirect
 	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
--- main.go
+++ main.go
@@ -2,25 +2,143 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/cli/go-gh"
+	"github.com/spf13/cobra"
 )
 
-func main() {
-	fmt.Println("hi world, this is the gh-md extension!")
-	client, err := gh.RESTClient(nil)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	response := struct {Login string}{}
-	err = client.Get("user", &response)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Printf("running as %s\n", response.Login)
+var rootCmd = &cobra.Command{
+	Short: "GitHub markdown link tools",
 }
 
-// For more examples of using go-gh, see:
-// https://github.com/cli/go-gh/blob/trunk/example_gh_test.go
+var linkCmd = &cobra.Command{
+	Use:   "link <url>",
+	Short: "Convert an input URL into a markdown link",
+	Long: `Convert an input URL into a markdown link.
+
+The input can be one of:
+
+   * a GitHub repository URL
+   * a GitHub issue URL
+   * a GitHub pull request URL
+   * a GitHub discussion URL
+   * a GitHub issue reference (e.g. "cli/cli#123")
+   * a markdown link containing one of the above URL types
+
+The output is a markdown link to the input URL, with the link text being the
+issue/PR/discussion reference as well as its title fetched from the GitHub API.
+
+Include the --simple flag to disable title lookup.
+
+If the input is unrecognized it will be returned as-is.
+
+Examples:
+
+   $ gh md link https://github.com/cli/cli/pull/123
+   [cli/cli#123: Tweak flags language](https://github.com/cli/cli/pull/123)
+
+   $ gh md link --simple https://github.com/cli/cli/pull/123
+   [cli/cli#123](https://github.com/cli/cli/pull/123)
+
+   $ gh md link cli/cli#123
+   [cli/cli#123: Tweak flags language](https://github.com/cli/cli/pull/123)`,
+
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		simple, _ := cmd.Flags().GetBool("simple")
+		out, err := link(args[0], simple)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		fmt.Println(out)
+	},
+}
+
+var refCmd = &cobra.Command{
+	Use:     "ref <url>",
+	Aliases: []string{"reference"},
+	Short:   "Convert an input URL into an issue reference",
+	Long: `Convert an input URL into an issue reference.
+
+The input can be one of:
+
+   * A GitHub issue URL
+   * A GitHub pull request URL
+   * A GitHub discussion URL
+   * An issue reference ("cli/cli#123")
+   * A markdown link containing one of the above
+
+Example:
+
+   $ gh md ref https://github.com/cli/cli/pulls/123
+   cli/cli#123`,
+	Run: func(cmd *cobra.Command, args []string) {
+		out, err := ref(args[0])
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		fmt.Println(out)
+	},
+}
+
+var titleCmd = &cobra.Command{
+	Use:   "title <url>",
+	Short: "Fetch the title of a GitHub issue, pull request, or discussion",
+	Long: `Fetch the title of a GitHub issue, pull request, or discussion.
+
+The input can be one of:
+
+   * A GitHub issue URL
+   * A GitHub pull request URL
+   * A GitHub discussion URL
+   * An issue reference ("cli/cli#123")
+   * A markdown link containing one of the above
+
+Example:
+
+   $ gh md title cli/cli#123
+   Tweak flags language`,
+	Run: func(cmd *cobra.Command, args []string) {
+		sanitize, _ := cmd.Flags().GetBool("sanitize")
+		out, err := title(args[0], sanitize)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		fmt.Println(out)
+	},
+}
+
+func init() {
+	linkCmd.Flags().BoolP("simple", "s", false, "Disable title lookup")
+	titleCmd.Flags().BoolP("sanitize", "s", false, "Sanitize output for use as a file path")
+	rootCmd.AddCommand(linkCmd, refCmd, titleCmd)
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func link(input string, simple bool) (string, error) {
+	client, err := gh.RESTClient(nil)
+	if err != nil {
+		return input, err
+	}
+	response := struct{ Login string }{}
+	err = client.Get("user", &response)
+	if err != nil {
+		return input, err
+	}
+	return fmt.Sprintf("running as %s", response.Login), nil
+}
+
+func ref(input string) (string, error) {
+	return input, nil
+}
+
+func title(input string, sanitize bool) (string, error) {
+	return input, nil
+}
